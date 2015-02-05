dde44d8b8dc01bf56a9f000b0e45feb73965baf1
Ronnie Holm <mail@bugfree.dk>
2015-02-05 23:10:38 +0100

    Update

--- Source/Exercises/Exercise01/Solution.cs
+++ Source/Exercises/Exercise01/Solution.cs
@@ -6,22 +6,6 @@ using System.Threading.Tasks;
 
 namespace Exercises.Exercise01
 {
-    /*
-        1. Given the Danish bill system of 1000, 500, 100, 50 
-           And coin system of 20, 10, 5, 2, 1
-           And dueAmount of 354
-           And customerPay of 500
-        2. Then totalChange = customerPay - dueAmount
-        3. Set remainingChange = totalChange
-        4. If remainingChange > 0
-        5.   typeOfChange = find the largest bill or coin just below remainingChange
-        6.   numberOfTypeOfChange = remainingChange / typeOfChange rounded down
-        7.   Output "numberOfTypeOfChange x typeOfChange"
-        8.   Reset remainingChange = remainingChange - typeOfChange * numberOfTypeOfChange
-        9.   Goto step 4
-        10. Done     
-     */
-
     class Solution
     {
     }

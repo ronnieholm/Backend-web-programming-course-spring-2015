98ce18308889878e34308c150e425bf635acf0e8
Ronnie Holm <mail@bugfree.dk>
2015-02-06 13:38:48 +0100

    Updated

--- Source/Exercises/Exercise02/Solution.cs
+++ Source/Exercises/Exercise02/Solution.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Exercises.Exercise02
 {
+    /*
+        1. Set current maximumAgeEncountered = 0
+        2. For each person in people                                    
+             If current person's age > maximumAgeEncountered
+             Then reset maximumAgeEncountered = current person's age
+        3. Done
+     */
+
     class Solution
     {
     }

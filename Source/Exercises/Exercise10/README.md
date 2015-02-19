d9a5b296cca11572de7e9e686ef8d496b297ac07
Ronnie Holm <mail@bugfree.dk>
2015-02-19 09:16:51 +0100

    Update

---
+++ Source/Exercises/Exercise10/README.md
@@ -0,0 +1,26 @@
+﻿# Exercise 10: Using a class
+
+This exercise is about using an existing Student class contained in
+the Student.cs file.
+
+1. Have a look inside the Student.cs file and see what the Student 
+   class definition contains (constructors, methods, etc). See if you 
+   can figure out what each method does.
+2. Now try to use the Student class yourself. Insert some new code
+   into the solution (in the Main method) which does the following: 
+
+   (a) Create a new student object
+   (b) Add a few test scores to the student 
+   (c) Print out the name and average test score for the 
+       student (use Console.WriteLine(...))
+3. Extend the code in the Student class so that it can also hold 
+   information about the country for the student. Specifically, 
+   you need to: 
+
+   (a) Add a new instance field to hold the country value 
+   (b) Change the constructor to include a country value in the 
+       parameter list. Also remember to use the new parameter to
+	   initialize the country instance field 
+   (c) Add a method GetCountry to return the country value
+4. Finally, test the new code by updating the code from part 2 
+   such that country information is included.
---
+++ Source/Exercises/Exercise10/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Exercise08
+{
+    class Student
+    {
+        private int[] _testScores;
+        private int _noOfTestsTaken;
+        private string _name;
+
+        // creates Student object with the provided name, number of tests set to zero,
+        // and with the ability to store 50 test scores.
+        public Student(string name)
+        {
+            this._name = name;
+            _noOfTestsTaken = 0;
+            _testScores = new int[50];
+        }
+
+        // adds a single test score which is a number between 0 and 100
+        public void AddTestScore(int score)
+        {
+            _testScores[_noOfTestsTaken] = score;
+            _noOfTestsTaken++;
+        }
+
+        // returns name of student
+        public String GetName()
+        {
+            return _name;
+        }
+
+        // returns number of tests student has taken
+        public int GetNumberOfTestsTaken()
+        {
+            return _noOfTestsTaken;
+        }
+
+        // calculates and return average value of scores for tests student has taken
+        public int GetAverageTestScore()
+        {
+            if (_noOfTestsTaken == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int sum = 0;
+                for (int index = 0; index < _noOfTestsTaken; index++)
+                {
+                    sum = sum + _testScores[index];
+                }
+                int average = sum / _noOfTestsTaken;
+                return average;
+            }
+        }
+    }
+}

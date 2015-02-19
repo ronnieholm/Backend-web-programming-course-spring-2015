faf5d46dab5cbad741bf2e15be7d61684022d7a4
Ronnie Holm <mail@bugfree.dk>
2015-02-19 11:39:15 +0100

    Update

--- Source/Exercises/Exercise09/Car.cs
+++ Source/Exercises/Exercise09/Car.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercises.Exercise09 {
+namespace Exercises.Exercise09
+{
     /* make use of class from Main method like so:
 
         Car myCar = new Car();

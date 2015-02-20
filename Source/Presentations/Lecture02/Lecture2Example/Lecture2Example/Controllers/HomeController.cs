06da7e5fb72ea177f34116b67985a1bbb0a909e1
Ronnie Holm <mail@bugfree.dk>
2015-02-20 21:17:49 +0100

    Update

---
+++ Source/Presentations/Lecture02/Lecture2Example/Lecture2Example/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture2Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            ViewBag.PasswordSuccesfullyChanged = false;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string newPassword, string repeatedPassword)
+        {
+            bool passwordChanged = false;
+
+            bool between6And40Characters = newPassword.Length >= 4 && newPassword.Length <= 60;
+            bool containLettersAndNumber = true;  // todo: implement real logic
+            bool containCprOrNemIdNumber = false; // todo: implement real logic
+
+            if (between6And40Characters &&
+                containLettersAndNumber && 
+                !containCprOrNemIdNumber &&
+                newPassword == repeatedPassword)
+            {
+                passwordChanged = true;
+            }
+            else
+            {
+                passwordChanged = false;
+            }
+
+            ViewBag.PasswordSuccesfullyChanged = passwordChanged;
+            return View();
+        }
+    }
+}
---
+++ Source/Presentations/Lecture02/Lecture2Example/Lecture2Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lecture2Example.Startup))]
+namespace Lecture2Example
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

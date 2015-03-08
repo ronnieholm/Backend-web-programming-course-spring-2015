40b3b54bb14b66b23c32e309eebe3b0243b1b4eb
Ronnie Holm <mail@bugfree.dk>
2015-03-08 13:05:44 +0100

    Update

---
+++ Source/Exercises/TheBankJob/README.md
@@ -0,0 +1,70 @@
+﻿# The Bank Job
+
+In this exercise we'll try to evolve parts of what could become a bank system. 
+Note that the exercise is different from previous ones in that the end goal 
+isn't as precisely defined. Your thoughts help shape the solution.
+
+The progression of the exercise is to start out from a set of vague requirements 
+and move toward a design for a set of classes. Then to try to code (and improve) 
+upon this set of classes and use those in a complete application.
+
+You'll have less guidance than usual and this means there's no one final or 
+correct solution. Some requirements stated in the assignment might not be clear. 
+You must then choose a way to interpret those.
+
+## The setup
+
+We wish to create an application for managing a bank. More specifically, for 
+a bank with three kinds of bank accounts:
+
+  - A "basic" account where you can deposit and withdraw money and retrieve the 
+    balance.
+  - A "savings" account which works like the "basic" account but with some 
+    extra features:
+	(a) You can only deposit money up to a certain limit.
+	(b) You can only withdraw money from the account three times. After the 
+	    third withdrawal there's a fee of 25 kr. for withdrawals.
+  - A "young limited" account which works like the "basic" account but with 
+    extra features:
+    (a) You can only withdraw a certain amount of money per day.
+	
+Furthermore, all bank accounts should include:
+
+  - A unique bank account number
+  - The name of the bank account owner
+
+## Step 1
+
+Think about (not code) how you could implement this application. Which classes 
+and instance are needed and how the methods would actually work. You can make 
+notes, draw diagrams, and so on, but you don't need to code anything just yet. 
+
+## Step 2
+
+Try to actually code the classes you came up with in step 1. Remember to also 
+create a few tests for the code you create. 
+
+Important: You don't need to think about inheritance in this step.
+
+## Step 3
+
+See if your code from step 2 can be improved by using inheritance. The classes 
+representing different kinds of bank accounts most likely have some things in 
+common. For instance, they have common instance fields and methods. Think about 
+how you can use inheritance to avoid declaring the same instance fields in many 
+different classes. See if you can use the fact that we can override the 
+implementation of a method, etc.
+
+## Step 4
+
+Develop a single, but probably a bit complex, class called Bank, which is supposed 
+to be the interface or entry point to the bank system. A programmer using the 
+bank system should thus not create bank account objects directly, but should do so 
+through the methods of the Bank class. In fact, it should be possible to do all 
+bank account operations through the Bank class, given the account number of a 
+specific account. 
+
+The Bank class must thus be able to create, store, and maintain bank account objects 
+(by store we mean while the program is running, not in a file or database). This'll 
+require that you think about what methods we need for the Bank class and how bank 
+account objects are stored appropriately inside the Bank class.
---
+++ Source/Exercises/TheBankJob/Solution.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// note: step 4 isn't part of this solution.
+
+namespace Exercises.TheBankJob
+{
+    class Runner
+    {
+        public void Run()
+        {
+            // test of basis account
+            Console.WriteLine();
+            Console.WriteLine("Test of basis account");
+            BasicAccountSolution basic = new BasicAccountSolution("Mr. Basic", 1);
+            PrintAccountInformation(basic);
+
+            basic.Deposit(3000);
+            PrintAccountInformation(basic);
+
+            basic.Deposit(3000);
+            basic.Deposit(3000);
+            basic.Deposit(3000);
+            basic.Deposit(3000);
+            PrintAccountInformation(basic);
+
+            basic.Withdraw(1000);
+            PrintAccountInformation(basic);
+
+            basic.Withdraw(1000);
+            basic.Withdraw(1000);
+            basic.Withdraw(1000);
+            basic.Withdraw(1000);
+            PrintAccountInformation(basic);
+
+            // test of savings account
+            Console.WriteLine();
+            Console.WriteLine("Test of savings account");
+            SavingsAccountSolution saving = new SavingsAccountSolution("Mr. Savings", 2, 10000);
+            PrintAccountInformation(saving);
+
+            saving.Deposit(3000);
+            PrintAccountInformation(saving);
+
+            saving.Deposit(3000);
+            PrintAccountInformation(saving);
+            saving.Deposit(3000);
+            PrintAccountInformation(saving);
+            saving.Deposit(3000);
+            PrintAccountInformation(saving);
+            saving.Deposit(3000);
+            PrintAccountInformation(saving);
+
+            saving.Withdraw(1000);
+            PrintAccountInformation(saving);
+
+            saving.Withdraw(1000);
+            PrintAccountInformation(saving);
+            saving.Withdraw(1000);
+            PrintAccountInformation(saving);
+            saving.Withdraw(1000);
+            PrintAccountInformation(saving);
+            saving.Withdraw(1000);
+            PrintAccountInformation(saving);
+
+            // test of young limited account
+            Console.WriteLine();
+            Console.WriteLine("Test of young limited account");
+            YoungLimitedAccountSolution young = new YoungLimitedAccountSolution("Mr. Limited", 3, 2000);
+            PrintAccountInformation(young);
+
+            young.Deposit(3000);
+            PrintAccountInformation(young);
+
+            young.Deposit(3000);
+            young.Deposit(3000);
+            young.Deposit(3000);
+            young.Deposit(3000);
+            PrintAccountInformation(young);
+
+            young.Withdraw(1000);
+            PrintAccountInformation(young);
+
+            young.Withdraw(1000);
+            PrintAccountInformation(young);
+            young.Withdraw(1000);
+            PrintAccountInformation(young);
+            young.DailyMaintenance();
+            young.Withdraw(1000);
+            PrintAccountInformation(young);
+            young.Withdraw(1000);
+            PrintAccountInformation(young);
+        }
+
+        public void PrintAccountInformation(BasicAccountSolution acc)
+        {
+            Console.WriteLine("Account no. {0} owned by {1}, has a balance of {2} kr.", acc.GetAccountNo(), acc.GetOwnerName(), acc.GetBalance());
+        }
+    }
+
+    // negative amounts aren't handled
+    public class BasicAccountSolution
+    {
+        private double _balance;
+        private string _owner;
+        private int _accountNumber;
+
+        public BasicAccountSolution(string owner, int accountNumber)
+        {
+            _owner = owner;
+            _accountNumber = accountNumber;
+        }
+
+        public virtual void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+
+        // assume each bank account have some sort of daily maintenance like calculating 
+        // interest rate, resetting limits, etc. Another class must call this method. It 
+        // isn't the bank account itself that detects the passing of a day.
+        public virtual void DailyMaintenance()
+        {
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public string GetOwnerName()
+        {
+            return _owner;
+        }
+
+        public int GetAccountNo()
+        {
+            return _accountNumber;
+        }
+    }
+
+    class SavingsAccountSolution : BasicAccountSolution
+    {
+        // deposit limit updated every time a deposit is made.
+        private double _depositLimit;
+        private int _numberOfWithdraws;
+
+        // as the exercise defines the values of these they are made constants. However,
+        // they could be instance fields initialised through the constructor instead.
+        private const int _numberOfWithdrawsLimit = 3;
+        private const double _withdrawFee = 25.0;
+
+        public SavingsAccountSolution(string owner, int accountNumber, double depositLimit)
+            : base(owner, accountNumber)
+        {
+            _depositLimit = depositLimit;
+        }
+
+        public override void Deposit(double amount)
+        {
+            // deposit only allowed if amount is lower than or equal to deposit limit
+            if (amount <= _depositLimit)
+            {
+                base.Deposit(amount);
+                _depositLimit -= amount;
+            }
+            else
+            {
+                Console.WriteLine("This deposit of {0} exceeds the current deposit limit of {1}", amount, _depositLimit);
+            }
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            _numberOfWithdraws++;
+
+            // if limit for withdrawals has been exceeded a fee is charged
+            if (_numberOfWithdraws > _numberOfWithdrawsLimit)
+            {
+                base.Withdraw(_withdrawFee);
+                Console.WriteLine("You have beed charged a fee of {0} for your withdrawal", _withdrawFee);
+            }
+        }
+    }
+
+    class YoungLimitedAccountSolution : BasicAccountSolution
+    {
+        private double _dailyWithdrawLimit;
+        private double _dailyWithdrawLeft;
+
+        public YoungLimitedAccountSolution(string owner, int accountNumber, double dailyWithdrawLimit)
+            : base(owner, accountNumber)
+        {
+            _dailyWithdrawLimit = dailyWithdrawLimit;
+            _dailyWithdrawLeft = dailyWithdrawLimit;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            // withdraw only allowed if amount is lower than or equal to the remaining withdraw limit
+            if (amount <= _dailyWithdrawLeft)
+            {
+                base.Withdraw(amount);
+                _dailyWithdrawLeft -= amount;
+            }
+            else
+            {
+                Console.WriteLine("The withdrawal of {0} kr. would cause you to exceed your daily withdraw limit", amount);
+            }
+        }
+
+        // resets back to the original withdraw limit
+        public override void DailyMaintenance()
+        {
+            base.DailyMaintenance();
+            _dailyWithdrawLeft = _dailyWithdrawLimit;
+        }
+    }
+}
